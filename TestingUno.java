3d490c39bf9f327bf2e00efe921a507d14feae56
wramosg4 <wramosg4@miumg.edu.gt>
2025-07-04 17:54:30 -0600

    testdos

--- TestingUno.java
+++
@@ -1,6 +0,0 @@
-public class TestingUno {
-
-    private int number;
-    
-
-}
---
+++ TestingUnos.java
@@ -0,0 +1,11 @@
+public class TestingUnos {
+
+    private int number;
+    public TestingUnos(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
